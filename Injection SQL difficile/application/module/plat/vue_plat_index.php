d3be98b812d5399f3fdcdb03904eefcaa4aa0f74
Aristide702'i <arikiller@live.com>
2024-06-19 09:47:13 +0200

    Injections_SQL

---
+++ Injection SQL difficile/application/module/plat/vue_plat_index.php
@@ -0,0 +1,31 @@
+    <h2>plat</h2>
+    <p><a class="btn btn-primary" href="<?= hlien("plat", "edit", "id", 0) ?>">Nouveau plat</a></p>
+    <form method="post">
+    	<div class='form-group'>
+    		<label for='chercheplat'>Rechercher un plat</label>
+    		<input id='chercheplat' name='chercheplat' type='text' size='50' value='<?= mhe($chercheplat) ?>' class='form-control' />
+    	</div>
+    	<input class="btn btn-success" type="submit" name="btChercher" value="Rechercher" />
+    </form>
+    <table class="table table-striped table-bordered table-hover">
+    	<thead>
+    		<tr>
+    			<th>pla_id</th>
+    			<th>pla_nom</th>
+    			<th>pla_calorie</th>
+    			<th>pla_prix</th>
+    			<th>modifier</th>
+    			<th>supprimer</th>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<?php
+			foreach ($data as $row) { ?>
+    			<tr>
+    				<?php foreach ($row as $valeur) { ?>
+    					<td><?= mhe($valeur) ?></td>
+    				<?php } ?>
+    			</tr>
+    		<?php } ?>
+    	</tbody>
+    </table>
